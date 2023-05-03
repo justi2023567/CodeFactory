bf180a4db8f099df80d6fac59cbe75f2045ba06f
mhoover0413 <79530557+mhoover0413@users.noreply.github.com>
2023-05-03 08:42:32 -0400

    Added Restart

---
+++ CodeFactory/Assets/Scripts/RaycastPoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastPoint : MonoBehaviour
+{
+    public GameObject player;
+
+    public float Y = 0;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.rotation = new Quaternion(90, 0, 0, 1);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(this.transform.position, -Vector3.up, out hit))
+        {
+            if (hit.distance < 2f)
+            {
+                if (Y == 0)
+                {
+                    Y = player.transform.position.y;
+                }
+                if (Y > player.transform.position.y)
+                {
+                    player.transform.position = new Vector3(player.transform.position.x, Y, player.transform.position.z);
+                }
+            }
+            else if (hit.distance > 2f)
+            {
+                Y = 0f;
+            }
+        }
+    }
+}

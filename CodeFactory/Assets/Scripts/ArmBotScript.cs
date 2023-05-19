994e76428b02e8b3d8b2e22e0b29f60d4dc79ef3
justi2023567 <82458643+justi2023567@users.noreply.github.com>
2023-05-19 14:56:48 -0400

    Arm bot grabs object

---
+++ CodeFactory/Assets/Scripts/ArmBotScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ArmBotScript : MonoBehaviour
+{
+
+    public GameObject Sphere;
+    public GameObject Target;
+
+    void Update()
+    {
+        if (Sphere != null)
+        {
+            Sphere.transform.position = Vector3.MoveTowards(Sphere.transform.position, Target.transform.position, 0.20f);
+        }
+
+        if (Sphere.transform.position == Target.transform.position)
+        {
+            Sphere.transform.position = new Vector3(0.0f, 0.0f, 4.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sphere")
+        {
+            Sphere = other.gameObject;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+    }
+
+}

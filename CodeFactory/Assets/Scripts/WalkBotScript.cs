ac5ccd3ff462333bbd65ee6d263a8c47cf7a10d2
Brand24376 <82458664+Brand24376@users.noreply.github.com>
2023-05-23 14:21:36 -0400

    Ore randomization system started
    ore has a randomized set location but there is a bug where both the walkbot and flybot pathfinding gets messed up

--- CodeFactory/Assets/Scripts/WalkBotScript.cs
+++ CodeFactory/Assets/Scripts/WalkBotScript.cs
@@ -66,7 +66,7 @@ public class WalkBotScript : MonoBehaviour {
                                 if (dist < minDist)
                                 {
                                     minDist = dist;
-                                    tMin = tt.transform;
+                                    tMin = tt.transform; 
                                 }
 
                             }

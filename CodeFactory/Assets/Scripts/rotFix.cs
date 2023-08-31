fc8d3f92350a3972bf6b4ff518012600607a961d
SkyBlade-Studios <93068545+SkyBlade-Studios@users.noreply.github.com>
2023-08-30 20:11:32 -0400

    Added Comments
    Added Comments to the scripts that needed them.

--- CodeFactory/Assets/Scripts/rotFix.cs
+++ CodeFactory/Assets/Scripts/rotFix.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class rotFix : MonoBehaviour
 {
-    // needs comments
     // Start is called before the first frame update
     void Start()
     {
+        //Sets this objects rotation to a new Quaternion (explained in PlayerMovement) with the parameters of this objects x, y, z, and w values
+        //This ensures that the rotation is exactly its own rotation not relative to the game world
         this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.position.y, this.transform.position.z, 1);
     }
 }

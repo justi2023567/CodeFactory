88411b4546fadad98671e5ff6bb3a657ef2e889e
Brand24376 <82458664+Brand24376@users.noreply.github.com>
2023-09-08 14:57:23 -0400

    Started on dialogue system, is currently broken

---
+++ CodeFactory/Assets/Scripts/NextDialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextDialogue : MonoBehaviour
+{
+    int index = 2;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && transform.childCount > 1)
+        {
+            if (PlayerController.dialogue)
+            {
+                transform.GetChild(index).gameObject.SetActive(true);
+                index += 1;
+                if (transform.childCount == index)
+                {
+                    index = 2;
+                    PlayerController.dialogue = false;
+                }
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- CodeFactory/Assets/Scripts/SalesCubeSystem.cs
+++ CodeFactory/Assets/Scripts/SalesCubeSystem.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SalesCubeSystem : MonoBehaviour
 {
     // Creates a keycode for the interact button
     public KeyCode InteractButton;
+
+    public GameObject d_template;
+    public GameObject canva;
 
     // Variable used to detect the player
     bool player_detection = false;
@@ -13,11 +17,25 @@ public class SalesCubeSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Checks if player_detection is true and if a certain key is pressed
-        if (player_detection && Input.GetKeyDown(InteractButton))
+        // Checks if player_detection is true and if a certain key is pressed and if dialogue is false (dialogue in PlayerController.cs)
+        if (player_detection && Input.GetKeyDown(InteractButton) && !PlayerController.dialogue)
         {
-            print("Dialogue triggered");
+            canva.SetActive(true);
+            // Enables the dialogue (dialogue becomes true)
+            PlayerController.dialogue = true;
+            NewDialogue("Hello There");
+            NewDialogue("This is a test");
+            NewDialogue("To see if this system works");
+            NewDialogue("Bye");
+            canva.transform.GetChild(1).gameObject.SetActive(true);
         }
+    }
+
+    void NewDialogue(string text)
+    {
+        GameObject template_clone = Instantiate(d_template, d_template.transform);
+        template_clone.transform.parent = canva.transform;
+        template_clone.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = text;
     }
 
     // Checks when something enters the sales cube sphere collider

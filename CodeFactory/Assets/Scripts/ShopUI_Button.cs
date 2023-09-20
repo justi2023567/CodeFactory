a108ffb54049d701fc47dd5c9ad00348d7ee37ea
Brand24376 <82458664+Brand24376@users.noreply.github.com>
2023-09-19 23:13:14 -0400

    Started on the buttons and made transparent packground for shopUI

---
+++ CodeFactory/Assets/Scripts/ShopUI_Button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUI_Button : MonoBehaviour
+{
+    public void buttonClicked()
+    {
+        Debug.Log("Button Among Us Clicked");
+    }
+}
